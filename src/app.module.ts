645307dad1f38ff7fcc75444f6e1bdffccc0ef3f
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-17 06:52:40 +0100

    Generate new "database" module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { getValidationEnvironmentVariables } from './config/environment-variables';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: getValidationEnvironmentVariables(),
     }),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [],
