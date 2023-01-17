7f5f89a1f513d853bc7bb089a5225461a3f9a3c0
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-17 06:23:47 +0100

    Add and use APP_PORT and APP_HOSTNAME

--- src/config/environment-variables.ts
+++ src/config/environment-variables.ts
@@ -8,5 +8,7 @@ export const getValidationEnvironmentVariables = () => {
     POSTGRES_DB: Joi.string().required(),
     POSTGRES_COLLATION: Joi.string().required(),
     POSTGRES_ENCODING: Joi.string().required(),
+    APP_PORT: Joi.number().required(),
+    APP_HOSTNAME: Joi.string().required(),
   });
 };
