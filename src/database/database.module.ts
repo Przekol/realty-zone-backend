29e7b5ab7a3a79c3395a828e7c009a78ad141507
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-17 08:56:27 +0100

    Add typeOrmConfig

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from '../config/typeorm.config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
+  ],
+})
 export class DatabaseModule {}
