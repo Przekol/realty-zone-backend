374db1f698bf1cef0bd0df9d13734415fa6896bc
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-27 18:08:35 +0100

    Add getByEmail method in UserService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async getByEmail(email: string): Promise<User> {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new HttpException(`User with email ${email} already exists`, HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+}
