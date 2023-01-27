f23843dc9d68b8c96fc83d93ef7d2cc3571c36e2
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-27 19:23:59 +0100

    Add register method

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { RegisterDto } from './dto/register.dto';
+import { hashData } from '../utils';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(private readonly usersService: UsersService) {}
+  async register(registrationData: RegisterDto) {
+    const hashPwd = await hashData(registrationData.password);
+
+    return await this.usersService.create({
+      ...registrationData,
+      hashPwd,
+    });
+  }
+}
