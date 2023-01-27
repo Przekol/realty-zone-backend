80760592ff064bc1a924d426690a207740177013
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-27 19:15:54 +0100

    Add type for error

--- src/utils/hash/checkHash.ts
+++ src/utils/hash/checkHash.ts
@@ -5,7 +5,7 @@ import { ErrorMessage } from '../messages';
 export const checkHash = async (data: string, encrypted: string): Promise<boolean> => {
   try {
     return await bcrypt.compare(data, encrypted);
-  } catch (error) {
+  } catch (error: unknown) {
     throw new HttpException(ErrorMessage.InternalServerError, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 };
--- src/utils/hash/hashData.ts
+++ src/utils/hash/hashData.ts
@@ -5,7 +5,7 @@ import { ErrorMessage } from '../messages';
 export const hashData = async (data: string): Promise<string> => {
   try {
     return await bcrypt.hash(data, 10);
-  } catch (error) {
+  } catch (error: unknown) {
     throw new HttpException(ErrorMessage.InternalServerError, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 };
