017934d9c5471a81cd04e5f8bbfa8f1b839b972b
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-29 17:07:41 +0100

    Add PassportModule import and LocalStrategy provider

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './strategies/local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, LocalStrategy],
 })
 export class AuthenticationModule {}
