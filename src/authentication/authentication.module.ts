e2178c545370a911ad930423f99966347cd5741d
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-31 16:24:45 +0100

    Add JwtModule import

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { CookieService } from './cookie.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, JwtModule.register({})],
   controllers: [AuthenticationController],
   providers: [AuthenticationService, LocalStrategy, CookieService],
 })
