91cc6a7c9429adcea1bc68c7077eb628634e6489
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-31 11:33:38 +0100

    Generate new "cookie" service

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -4,10 +4,11 @@ import { AuthenticationService } from './authentication.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { CookieService } from './cookie.service';
 
 @Module({
   imports: [UsersModule, PassportModule],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService, LocalStrategy],
+  providers: [AuthenticationService, LocalStrategy, CookieService],
 })
 export class AuthenticationModule {}
