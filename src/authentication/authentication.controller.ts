74f34c16dc7a5d0f261a5b9fceb8ba255f805125
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-01-31 16:27:14 +0100

    Add cookie setting in login method

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, HttpCode, Post, Res, UseGuards } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthenticationService } from './authentication.service';
 import { RegisterDto } from './dto/register.dto';
 import { LocalAuthenticationGuard } from './guards/localAuthentication.guard';
+import { ConfigService } from '@nestjs/config';
+import { CurrentUser } from '../decorators/current-user.decorator';
+import { CookiesNames, UserEntity } from '../types';
+import { CookieService } from './cookie.service';
 
 @Controller('authentication')
 export class AuthenticationController {
-  constructor(private readonly authenticationService: AuthenticationService) {}
+  constructor(
+    private readonly authenticationService: AuthenticationService,
+    private readonly configService: ConfigService,
+    private readonly cookiesService: CookieService,
+  ) {}
 
   @Post('signup')
   async register(@Body() registrationData: RegisterDto) {
@@ -16,8 +24,16 @@ export class AuthenticationController {
   @HttpCode(200)
   @UseGuards(LocalAuthenticationGuard)
   @Post('signin')
-  async login(@Req() req: Request) {
-    const user = req.user;
+  async login(@CurrentUser() user: UserEntity, @Res({ passthrough: true }) res: Response) {
+    const token = this.authenticationService.getJwtToken(
+      { id: user.id },
+      this.configService.get('JWT_SECRET_ACCESS'),
+      this.configService.get('JWT_EXPIRATION_TIME_ACCESS'),
+    );
+    this.cookiesService.setTokenInCookie(res, CookiesNames.AUTHENTICATION, {
+      token,
+      expiresIn: this.configService.get('JWT_EXPIRATION_TIME_ACCESS'),
+    });
     return user;
   }
 }
