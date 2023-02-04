af6a295a52888516379214f5d777e61409b2637e
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-04 17:31:50 +0100

    Add '/profile' endpoint to test user authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { RoleGuard } from '../authentication/guards/role.guard';
-import { Role } from '../../@types';
+import { Role, UserEntity } from '../../@types';
+import { CurrentUser } from '../decorators/current-user.decorator';
 
+@UseGuards(RoleGuard(Role.User))
 @Controller('users')
 export class UsersController {
   @UseGuards(RoleGuard(Role.Admin))
@@ -9,6 +11,12 @@ export class UsersController {
   async getUsers() {
     return {
       users: 'tutaj jest lista',
+    };
+  }
+  @Get('/profile')
+  async getUser(@CurrentUser() user: UserEntity) {
+    return {
+      user,
     };
   }
 }
