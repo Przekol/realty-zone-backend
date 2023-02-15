f1ed016055f2430658b04a3e2480a4425d17ea06
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-02-15 19:43:19 +0100

    Add 'confirm' endpoint and implement confirm method

--- src/email-confirmation/email-confirmation.controller.ts
+++ src/email-confirmation/email-confirmation.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+
+import { ConfirmEmailDto } from './dto/confirm-email.dto';
+import { EmailConfirmationService } from './email-confirmation.service';
 
 @Controller('email-confirmation')
-export class EmailConfirmationController {}
+export class EmailConfirmationController {
+  constructor(private readonly emailConfirmationService: EmailConfirmationService) {}
+
+  @HttpCode(200)
+  @Post('confirm')
+  async confirm(@Body() confirmationData: ConfirmEmailDto) {
+    const email = await this.emailConfirmationService.decodeConfirmationToken(confirmationData.token);
+    await this.emailConfirmationService.confirmEmail(email);
+  }
+}
