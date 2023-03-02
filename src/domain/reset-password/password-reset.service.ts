f17b32fcd150f4989b5477ee1d6c1dfbeaad5f20
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-02 22:11:25 +0100

    Add createPasswordResetToken method

--- src/domain/reset-password/password-reset.service.ts
+++ src/domain/reset-password/password-reset.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 
 import { PasswordResetToken } from '@domain/reset-password/entities';
 import { User } from '@domain/users/entities';
+import { hashData } from '@shared/utils';
 
 @Injectable()
 export class PasswordResetService {
@@ -12,6 +14,15 @@ export class PasswordResetService {
     if (token && token.expiresIn < Date.now()) {
       return null;
     }
+    return token;
+  }
+
+  async createPasswordResetToken(user: User): Promise<PasswordResetToken> {
+    const token = new PasswordResetToken();
+    token.hashToken = await hashData(uuid());
+    await token.save();
+    token.user = user;
+    await token.save();
     return token;
   }
 }
