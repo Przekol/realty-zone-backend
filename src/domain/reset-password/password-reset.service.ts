db82a24984e86472b0893dbc7b2a56c7af634865
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-02 15:53:54 +0100

    Add getActivePasswordResetTokenForUser method

--- src/domain/reset-password/password-reset.service.ts
+++ src/domain/reset-password/password-reset.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+import { PasswordResetToken } from '@domain/reset-password/entities';
+import { User } from '@domain/users/entities';
+
 @Injectable()
-export class PasswordResetService {}
+export class PasswordResetService {
+  async getActivePasswordResetTokenForUser(user: User): Promise<PasswordResetToken | null> {
+    const token = await PasswordResetToken.findOne({
+      where: { user: { id: user.id }, isUsed: false },
+    });
+    if (token && token.expiresIn < Date.now()) {
+      return null;
+    }
+    return token;
+  }
+}
