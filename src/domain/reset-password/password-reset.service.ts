b135c99573f1677f9f0778cfff15c2f1e3ea108e
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-03 08:44:11 +0100

    Add generateResetTokenLink method

--- src/domain/reset-password/password-reset.service.ts
+++ src/domain/reset-password/password-reset.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DataSource, MoreThan } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
@@ -8,7 +9,7 @@ import { hashData } from '@shared/utils';
 
 @Injectable()
 export class PasswordResetService {
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(private readonly dataSource: DataSource, private readonly configService: ConfigService) {}
 
   async isResetTokenActiveForUser(user: User): Promise<boolean> {
     const resetToken = await PasswordResetToken.findOne({
@@ -25,7 +26,12 @@ export class PasswordResetService {
     await this.dataSource.manager.save(passwordResetToken);
     return token;
   }
+
   private async generateResetToken(): Promise<string> {
     return uuid();
+  }
+
+  async generateResetTokenLink(token: string, userId: string): Promise<string> {
+    return `${this.configService.get('CLIENT_URL')}/reset-password?token=${token}&id=${userId}`;
   }
 }
