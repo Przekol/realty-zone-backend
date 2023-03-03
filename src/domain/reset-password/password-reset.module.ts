b414f51c96ef1ab860319efdaa53e330b0514af4
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-03 12:23:43 +0100

    Add PasswordResetService export  and forwardRef EventsModule module.

--- src/domain/reset-password/password-reset.module.ts
+++ src/domain/reset-password/password-reset.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { PasswordResetToken } from '@domain/reset-password/entities/password-reset-token.entity';
+import { PasswordResetToken } from '@domain/reset-password/entities';
 import { UsersModule } from '@domain/users';
 import { EmailModule } from '@providers/email';
+import { EventsModule } from '@providers/event-emitter/events.module';
 
 import { PasswordResetController } from './password-reset.controller';
 import { PasswordResetService } from './password-reset.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PasswordResetToken]), UsersModule, EmailModule],
+  imports: [TypeOrmModule.forFeature([PasswordResetToken]), UsersModule, EmailModule, forwardRef(() => EventsModule)],
   controllers: [PasswordResetController],
   providers: [PasswordResetService],
+  exports: [PasswordResetService],
 })
 export class PasswordResetModule {}
