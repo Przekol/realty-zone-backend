820d5ae9567a46ce38691547aae9a5b7c7537494
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-03 09:22:02 +0100

    Add sendResetPasswordLink method

--- src/domain/reset-password/password-reset.module.ts
+++ src/domain/reset-password/password-reset.module.ts
@@ -3,12 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { PasswordResetToken } from '@domain/reset-password/entities/password-reset-token.entity';
 import { UsersModule } from '@domain/users';
+import { EmailModule } from '@providers/email';
 
 import { PasswordResetController } from './password-reset.controller';
 import { PasswordResetService } from './password-reset.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PasswordResetToken]), UsersModule],
+  imports: [TypeOrmModule.forFeature([PasswordResetToken]), UsersModule, EmailModule],
   controllers: [PasswordResetController],
   providers: [PasswordResetService],
 })
--- src/domain/reset-password/password-reset.service.ts
+++ src/domain/reset-password/password-reset.service.ts
@@ -5,11 +5,16 @@ import { v4 as uuid } from 'uuid';
 
 import { PasswordResetToken } from '@domain/reset-password/entities';
 import { User } from '@domain/users/entities';
+import { EmailService } from '@providers/email';
 import { hashData } from '@shared/utils';
 
 @Injectable()
 export class PasswordResetService {
-  constructor(private readonly dataSource: DataSource, private readonly configService: ConfigService) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly configService: ConfigService,
+    private readonly emailService: EmailService,
+  ) {}
 
   async isResetTokenActiveForUser(user: User): Promise<boolean> {
     const resetToken = await PasswordResetToken.findOne({
@@ -33,5 +38,16 @@ export class PasswordResetService {
 
   async generateResetTokenLink(token: string, userId: string): Promise<string> {
     return `${this.configService.get('CLIENT_URL')}/reset-password?token=${token}&id=${userId}`;
+  }
+
+  async sendResetPasswordLink(user: User, subject: string) {
+    const token = await this.createPasswordResetToken(user);
+    const resetTokenLink = await this.generateResetTokenLink(token, user.id);
+
+    await this.emailService.sendMail(user.email, subject, 'authentication/password-reset', {
+      username: user.username,
+      url: resetTokenLink,
+      title: subject,
+    });
   }
 }
