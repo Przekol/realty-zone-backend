b99f971d64baff4242b7aadd4eb2f77219aea15a
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-03-06 13:06:47 +0100

    Add 'welcome' template

--- src/providers/email/types.ts
+++ src/providers/email/types.ts
@@ -2,4 +2,5 @@ export enum MailTemplate {
   emailConfirmation = 'authentication/email-confirmation',
   passwordReset = 'authentication/password-reset',
   passwordResetSuccess = 'authentication/password-reset-success',
+  welcome = 'authentication/welcome',
 }
