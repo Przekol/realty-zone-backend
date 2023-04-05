d6723ac7931c0b7171ed7703eec48ed7bfbb7bdf
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-05 22:49:04 +0200

    Implement GetAllOffers method

--- src/api/offers/offers.controller.ts
+++ src/api/offers/offers.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+
+import { OfferResponse } from '@types';
+
+import { PaginationOptionsDto } from './dto';
+import { OffersService } from './offers.service';
 
 @Controller('offers')
-export class OffersController {}
+export class OffersController {
+  constructor(private readonly offersService: OffersService) {}
+  @Get('/')
+  getAllOffers(@Query() paginationOptionsDto: PaginationOptionsDto): Promise<OfferResponse> {
+    return this.offersService.getAllOffers(paginationOptionsDto);
+  }
+}
--- src/api/offers/offers.service.ts
+++ src/api/offers/offers.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
 
+import { Offer } from '@api/offers/entities';
+
+import { OfferResponse } from '@types';
+
+import { PaginationOptionsDto } from './dto';
+
 @Injectable()
-export class OffersService {}
+export class OffersService {
+  async getAllOffers(paginationOptionsDto: PaginationOptionsDto): Promise<OfferResponse> {
+    const { page = 1, limit = 10, sortOrder = 'DESC' } = paginationOptionsDto;
+    const skip = (page - 1) * limit;
+
+    const [offers, total] = await Offer.findAndCount({
+      skip,
+      take: limit,
+      order: {
+        createdAt: sortOrder,
+      },
+    });
+
+    return {
+      offers,
+      pagination: {
+        currentPage: page,
+        itemsPerPage: limit,
+        totalPages: Math.ceil(total / limit),
+        totalItems: total,
+      },
+    };
+  }
+}
