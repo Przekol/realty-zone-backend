83bb297e83bf131574961587510cb7f10fce36e3
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-05 11:47:46 +0200

    Generate new 'offers' module

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { OffersModule } from '@api//offers/offers.module';
 import { AuthenticationModule } from '@api/authentication/authentication.module';
 import { EmailConfirmationModule } from '@api/email-confirmation/email-confirmation.module';
 import { PasswordResetModule } from '@api/password-reset/password-reset.module';
 import { UsersModule } from '@api/users/users.module';
 
 @Module({
-  imports: [AuthenticationModule, EmailConfirmationModule, PasswordResetModule, UsersModule],
+  imports: [AuthenticationModule, EmailConfirmationModule, PasswordResetModule, UsersModule, OffersModule],
 })
 export class ApiModule {}
---
+++ src/api/offers/offers.controller.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { OffersController } from './offers.controller';
+
+describe('OffersController', () => {
+  let controller: OffersController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [OffersController],
+    }).compile();
+
+    controller = module.get<OffersController>(OffersController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
