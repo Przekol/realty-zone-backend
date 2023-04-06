2e27d2add027b9b477ead1bab56fc4c9995c9c28
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-06 16:20:12 +0200

    Add findDictionaries method

--- src/providers/dictionaries/dictionaries.service.ts
+++ src/providers/dictionaries/dictionaries.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+import { FindDictionariesDto } from '@providers/dictionaries/dto/find-dictionaries.dto';
+import { Market, Ownership, Status, Transaction, Types } from '@providers/dictionaries/entities/dictionary.entity';
+
 @Injectable()
-export class DictionariesService {}
+export class DictionariesService {
+  async findDictionaries(findDictionariesDto: FindDictionariesDto) {
+    return await Promise.all([
+      Market.findOneByOrFail({ id: findDictionariesDto.market }),
+      Transaction.findOneByOrFail({ id: findDictionariesDto.transaction }),
+      Ownership.findOneByOrFail({ id: findDictionariesDto.ownership }),
+      Status.findOneByOrFail({ id: findDictionariesDto.status }),
+      Types.findOneByOrFail({ id: findDictionariesDto.type }),
+    ]);
+  }
+}
