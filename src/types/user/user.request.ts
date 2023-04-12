bd241f9aa44fa6461836bc56caf3c7c60cead066
Przemek <przemyslaw.r.wojciechowski@gmail.com>
2023-04-12 16:32:07 +0200

    Fixbug

--- src/types/user/user.request.ts
+++ src/types/user/user.request.ts
@@ -4,3 +4,9 @@ export type CreateUserRequest = Pick<UserEntity, 'email' | 'hashPwd'>;
 
 export type RegisterUserRequest = Omit<CreateUserRequest, 'hashPwd'> & { password: string };
 export type LoginUserRequest = Pick<RegisterUserRequest, 'email' | 'password'>;
+export type UpdateUserRequest = {
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+};
